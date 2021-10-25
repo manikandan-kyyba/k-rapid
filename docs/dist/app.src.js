dfdd5a1423916ab1bf5fc041fe139cf7fd00f1de
Manikandan Ubuntu <manikandana@kyyba.com>
2021-10-25 14:09:30 +0530

    ok

---
+++ docs/dist/app.src.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RedBox from 'redbox-react';
+import App from '/home/ubuntu/react-native-kyyba/docs/node_modules/component-docs/dist/templates/App.js';
+import data from './app.data';
+import '/home/ubuntu/react-native-kyyba/docs/node_modules/component-docs/dist/styles/reset.css';
+import '/home/ubuntu/react-native-kyyba/docs/node_modules/component-docs/dist/styles/globals.css';
+
+import '/home/ubuntu/react-native-kyyba/docs/assets/styles.css';
+
+const root = document.getElementById('root');
+const render = () => {
+  try {
+    ReactDOM.hydrate(
+      <App
+        name={window.__INITIAL_PATH__}
+        data={data}
+        github={"https://github.com/manikandan-kyyba/react-native-kyyba/edit/main/"}
+        logo={"images/sidebar-logo.svg"}
+        title={"[title] · React Native Kyyba"}
+      />,
+      root
+    );
+  } catch (e) {
+    ReactDOM.render(
+      <RedBox error={e} />,
+      root
+    );
+  }
+};
+
+if (module.hot) {
+  module.hot.accept(() => {
+    render();
+  });
+}
+
+render();
